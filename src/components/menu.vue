762d7a1db76e05b1be9edaffe532d12075658c60
sixiaopei <sixiaopei@dodoca.com>
2017-12-04 16:17:10 +0800

    1

---
+++ src/components/menu.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="menu">
+			<ul>
+				<li>首页</li>
+				<li>最新</li>
+				<li>最热</li>
+			</ul>
+	</div>
+
+</template>
+<script type="text/javascript">
+
+	export default{
+			data(){
+				return {
+                
+				}
+			}
+
+
+	}
+
+
+</script>
+<style scoped>
+
+	.menu{height: 100px;background: #333;}
+	.menu ul li{display: inline-block;font-size: 22px;color: #fff;padding:0 20px;line-height: 100px;}
+
+</style>
