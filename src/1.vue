a08e21235c37bb5a9eaf8094326f129feb692c0d
sixiaopei <sixiaopei@dodoca.com>
2018-01-04 16:25:03 +0800

    11

---
+++ src/1.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    
+  
+  <v-menu ></v-menu>
+  </div>
+</template>
+
+<script>
+import menu from './components/menu.vue'
+export default {
+  components:{'v-menu':menu},
+  data(){
+    return {
+
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+}
+
+</style>
